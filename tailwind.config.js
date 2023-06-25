28a139cb8eedf1a2c870098e8721cd76c81d3306
dinosoft-web <123156149+dinosoft-web@users.noreply.github.com>
2023-06-25 16:49:34 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		colors: {
+			main: '#4A4A4A',
+			secondary: '#8D8F8E',
+			tertiary: '#7F8FA4',
+			backgroud: '#EFEFEF',
+			greenish: '#71827A',
+			white: '#fff',
+		},
+		fontFamily: {
+			serif: ['"Cormorant Garamond"'],
+			body: ['"Open Sans"'],
+		},
+		extend: {},
+	},
+	plugins: [],
+}
