c20bcd54cb986f1654a5ccb044b1398a3a3daca7
dinosoft-web <123156149+dinosoft-web@users.noreply.github.com>
2023-06-27 17:25:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,71 @@
 const tabs = document.querySelectorAll('.tab')
 const panels = document.querySelectorAll('.panel')
+const arrowsRight = document.querySelectorAll('.arrow-right')
+const arrowsLeft = document.querySelectorAll('.arrow-left')
+const imageContainers = document.querySelectorAll('.image-container')
+const suiteImages = document.querySelectorAll('.suite-image')
+
+BACKGROUND_IMAGES = [
+	[
+		'../assets/pool-night.jpeg',
+		'../assets/outdoor1.jpg',
+		'../assets/outdoor2.JPG',
+		'../assets/outdoor3.jpg',
+	],
+	[
+		'../assets/pool-night.jpeg',
+		'../assets/outdoor1.jpeg',
+		'../assets/outdoor2.jpeg',
+		'../assets/outdoor3.jpeg',
+	],
+	[
+		'../assets/pool-night.jpeg',
+		'../assets/outdoor1.jpeg',
+		'../assets/outdoor2.jpeg',
+		'../assets/outdoor3.jpeg',
+	],
+]
+
+let currentImage = 0
+
+suiteImages.forEach((image, index) => {
+	image.addEventListener('click', () => {
+		if (!suiteImages[index].classList.contains('left-0')) {
+			suiteImages[index].classList.replace('left-[230px]', 'left-0')
+			suiteImages[index - 1].classList.replace('left-0', '-left-[1050px]')
+			suiteImages[index + 1].classList.replace('left-[460px]', 'left-[230px]')
+		}
+	})
+})
+
+// Arrows event Listener
+arrowsRight.forEach((arrowRight, index) =>
+	arrowRight.addEventListener('click', () => {
+		if (currentImage <= BACKGROUND_IMAGES[index].length - 2) currentImage++
+		imageContainers[
+			index
+		].style.backgroundImage = `url('${BACKGROUND_IMAGES[index][currentImage]}')`
+	})
+)
+
+arrowsLeft.forEach((arrowLeft, index) =>
+	arrowLeft.addEventListener('click', () => {
+		if (currentImage >= 1) {
+			currentImage--
+			imageContainers[
+				index
+			].style.backgroundImage = `url('${BACKGROUND_IMAGES[index][currentImage]}')`
+		}
+	})
+)
+
+arrowsLeft.forEach((arrowLeft) =>
+	arrowLeft.addEventListener('click', onArrowLeftClick)
+)
+
+function onArrowRigthClick() {}
+
+function onArrowLeftClick() {}
 
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
@@ -7,6 +73,7 @@ tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
 function onTabClick(e) {
 	// Deactivate all tabs
 	tabs.forEach((tab) => {
+		currentImage = 0
 		tab.children[0].classList.remove(
 			'border-secondary',
 			'border-b-4',
